5bdb048cdc2a21afd19140bf82adc96d51c1874c
Marcus Johansson <majohansson@gmail.com>
2013-01-19 16:06:54 +0100

    started on media files

---
+++ Socially/ViewModels/MediaFileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using ScApi.Data;
+
+namespace Socially.ViewModels
+{
+    public class MediaFileViewModel
+    {
+        private readonly MediaFile _mediaFile;
+
+        public MediaFileViewModel(MediaFile mediaFile)
+        {
+            _mediaFile = mediaFile;
+        }
+
+        public string ThumbnailUrl
+        {
+            get { return _mediaFile.Thumbnails.Stream; }
+        }
+
+        public string Url
+        {
+            get { return _mediaFile.Url; }
+        }
+    }
+}
